bc99dfc80ab0bbb845f5fbc23c1b3c137756d7f3
ywonchae1 <ywonchae62@gmail.com>
2023-04-26 20:41:05 +0900

    TemplateLiteral

---
+++ syntax/variable2.js
@@ -0,0 +1,4 @@
+var name = 'egoing';
+var letter = 'Dear ' + name + 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed enim dolor, suscipit quis felis ut, fringilla ' + name + 'viverra massa. Fusce cursus est in ante sodales ultricies. Sed mattis, dui quis tempus accumsan, leo neque rhoncus leo, a sodales ante eros at felis. Maecenas nunc' + name + ' elit, elementum ut sapien nec, commodo malesuada lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed laoreet orci in arcu laoreet semper. Pellentesque sit amet imperdiet sem, eu interdum purus.';
+
+console.log(letter);
