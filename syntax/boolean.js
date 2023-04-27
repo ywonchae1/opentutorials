4ff3f425a573aedf1cb3f2a0fd52ddc3fb357337
ywonchae1 <ywonchae62@gmail.com>
2023-04-27 10:51:53 +0900

    class19

---
+++ syntax/boolean.js
@@ -0,0 +1,4 @@
+console.log(true);
+console.log(false);
+var a = 1;
+//true = 1;
---
+++ syntax/comparison.js
@@ -0,0 +1,7 @@
+console.log(1+1);
+console.log(1 == 1);
+console.log(1 == 2);
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 === 3);
+console.log(1 === 1);
