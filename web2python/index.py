e2dc5b9a9e150321c6675be769dc255eb9f535d7
ywonchae1 <ywonchae62@gmail.com>
2023-06-21 08:53:27 +0900

    python complete

--- web2python/index.py
+++ web2python/index.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 print("Content-Type: text/html\n")
 import cgi, os, view
+from html_sanitizer import Sanitizer
+sanitizer = Sanitizer()
 
 form = cgi.FieldStorage()
-pageId = "Welcome"
+title = pageId = "Welcome"
 description = 'Hello world!'
 update_link = ''
 delete_action = ''
 if 'id' in form:
-    pageId = form["id"].value
+    title = pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
-    description = description.replace('<', '&lt;')
-    description = description.replace('>', '&gt;')
+    #description = description.replace('<', '&lt;')
+    #description = description.replace('>', '&gt;')
+    description = sanitizer.sanitize(description)
+    title = sanitizer.sanitize(title)
     update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
     delete_action = '''
         <form action="process_delete.py" method="post">
@@ -41,7 +45,7 @@ print('''
   </p>
 </body>
 </html>
-'''.format(title=pageId,
+'''.format(title=title,
 description=description,
 update_link=update_link,
 delete_action=delete_action,
